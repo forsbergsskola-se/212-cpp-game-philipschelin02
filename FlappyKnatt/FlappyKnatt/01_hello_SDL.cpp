81676e4986225dfe2d16ede9f79cb5e926922687
philipschelin02 <112477616+philipschelin02@users.noreply.github.com>
2023-03-28 15:33:41 +0200

    Cleaned up the code. Tried to do the second assignment but it doesn't tell me anything. Either I'm dumb, unskilled, or the tutorial is bad at specifying what it wants me to do.

--- FlappyKnatt/FlappyKnatt/01_hello_SDL.cpp
+++ FlappyKnatt/FlappyKnatt/01_hello_SDL.cpp
@@ -12,13 +12,15 @@ const int SCREEN_HEIGHT = 480;
 int main( int argc, char* args[] )
 {
 	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	SDL_Window* window{};
 	
 	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	SDL_Surface* screenSurface{};
+
+
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	if( SDL_Init( SDL_INIT_VIDEO ) )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
@@ -26,24 +28,29 @@ int main( int argc, char* args[] )
 	{
 		//Create window
 		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
+		if( !window )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			return -1;
 		}
-		else
-		{
+	
 			//Get window surface
 			screenSurface = SDL_GetWindowSurface( window );
 
 			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			SDL_FillRect( screenSurface, nullptr, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 			
 			//Update the surface
 			SDL_UpdateWindowSurface( window );
             
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
+            SDL_Event e; bool quit = false; 
+			while( !quit ){ 
+				while( SDL_PollEvent( &e ) ){ 
+					if( e.type == SDL_QUIT ) 
+						quit = true; } 
+				}
+
 	}
 
 	//Destroy window
