6747cc9eeaab513d96e8d7d927664fa249e49e26
philipschelin02 <112477616+philipschelin02@users.noreply.github.com>
2023-04-03 10:36:26 +0200

    Hmmmm, I have quite an idea for a game... Marc, prepare to be impressed.

--- FlappyKnatt/FlappyKnatt/01_hello_SDL.cpp
+++ FlappyKnatt/FlappyKnatt/01_hello_SDL.cpp
@@ -4,60 +4,56 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include "Window.h"
+#include "Image.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window{};
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface{};
-
-
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) )
+	Window window{ SCREEN_WIDTH, SCREEN_HEIGHT };
+	//Start up SDL and create window
+	if (!window.wasSuccessful())
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		printf("Failed to initialize!\n");
+		return -1;
 	}
-	else
+
+
+	//Load media
+	Image image{"img/hello_world.bmp"};
+	if (!image.wasSuccessful())
 	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( !window )
+		printf("Failed to load media!\n");
+		return -1;
+	}
+
+	window.render(image);
+
+	// while(!window.quit())
+	//     window.update();
+
+	//Hack to get window to stay up
+	SDL_Event e; bool quit = false;
+	while (quit == false)
+	{
+		while (SDL_PollEvent(&e))
 		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-			return -1;
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
 		}
-	
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
-
-			//Fill the surface white
-			SDL_FillRect( screenSurface, nullptr, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
-            
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; 
-			while( !quit ){ 
-				while( SDL_PollEvent( &e ) ){ 
-					if( e.type == SDL_QUIT ) 
-						quit = true; } 
-				}
-
 	}
-
-	//Destroy window
-	SDL_DestroyWindow( window );
-
-	//Quit SDL subsystems
-	SDL_Quit();
 
 	return 0;
 }
+
+
+
+
+
+
+
